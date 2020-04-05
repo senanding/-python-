e66d0329eaeafa3291a07a14089fdce15e1b5aee
senanding <61817631+senanding@users.noreply.github.com>
2020-04-05 15:09:28 +0800

    Add files via upload

--- login/myauth/views.py
+++ login/myauth/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.forms import UserCreationForm,UserChangeForm
-from .forms import DIYUserCreationForm
+from .forms import DIYUserCreationForm,DIYUserChangeForm
 from .models import formalVIP
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
 
 # Create your views here.
 
@@ -16,22 +17,33 @@ def userCenter(request):#用户中心
 @login_required(login_url='myauth:login')
 def editorProfile(request):#编辑个人信息
     if request.method == "POST":
-        editorForm = UserChangeForm(request.POST,instance=request.user)#你要修改谁要写清楚
+        editorForm = DIYUserChangeForm(request.POST,instance=request.user)#你要修改谁要写清楚
         if editorForm.is_valid():
+            request.user.formalvip.nick = editorForm.cleaned_data['nick']
+            request.user.formalvip.birthday = editorForm.cleaned_data['birthday']
+            request.user.formalvip.save()
             editorForm.save()  # 创建并保存,用户信息已经改变了
             return redirect('myauth:userCenter')
     else:
-        editorForm = UserChangeForm()
+        editorForm = DIYUserChangeForm(instance=request.user)
     context = {}
     context["editorForm"] = editorForm
+    context["user"] = request.user
     return render(request, 'myauth/editorProfile.html', context)
 
 @login_required(login_url='myauth:login')
 def changePassword(request):# 更改密码
-    if request.method=="POST":
-        return redirect('myauth:userCenter')
+    if request.method == "POST":
+        changePwdForm = PasswordChangeForm(data=request.POST, user=request.user)  # 你要修改谁要写清楚
+        if changePwdForm.is_valid():
+            changePwdForm.save()  # 创建并保存,用户信息已经改变了
+            return redirect('myauth:loginpage')
     else:
-        return render(request,'myauth/changePassword.html')
+        changePwdForm = PasswordChangeForm(user=request.user)
+    context = {}
+    context["changePwdForm"] = changePwdForm
+    # context["user"] = request.user
+    return render(request,'myauth/changePassword.html',context)
 
 def index(request):
     return render(request,'myauth/home.html')
@@ -66,7 +78,7 @@ def myregister(request):
             registerForm.save()
             user=authenticate(username=registerForm.cleaned_data["username"],password=registerForm.cleaned_data["password1"])
             user.email = registerForm.cleaned_data['email']
-            formalVIP(user=user, nick=registerForm.cleaned_data['昵称'],birthday=registerForm.cleaned_data['生日']).save()
+            formalVIP(user=user, nick=registerForm.cleaned_data['nick'],birthday=registerForm.cleaned_data['birthday']).save()
             login(request,user)
             return redirect('myauth:index')
     else:
